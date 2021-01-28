7e3348cfc0661d5825786a0e64261fd81f5f6119
yongyi <yongyiwoo@gmail.com>
2021-01-28 22:15:52 +0100

    Update the model name element

--- page_object/device_connection_pages.py
+++ page_object/device_connection_pages.py
@@ -4,7 +4,7 @@ from page_object.base_page import BasePage
 class DeviceConnectionPages(BasePage):
     def __init__(self, common_driver):
         super(DeviceConnectionPages, self).__init__(common_driver)
-        self.model_list = (MobileBy.XPATH, "//androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup")
+        self.model_name_list = (MobileBy.ID, "com.blueair.android:id/model_name")
 
     def tap_device_model(self):
         pass
@@ -12,7 +12,7 @@ class DeviceConnectionPages(BasePage):
     def find_device_page(self):
         pass
 
-    def connect_wifi_page(self, password):
+    def connect_wifi_page(self, ssid, password):
         pass
 
     def name_device_page(self, name):
